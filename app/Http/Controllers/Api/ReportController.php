033a8f1ac6dbd63c085450b68e0d263e8b19aca1
Woody Evins <woody.evins@gmail.com>
2020-06-04 14:37:48 -0400

    add basic reporting

---
+++ app/Http/Controllers/Api/ReportController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ReportController extends Controller
+{
+    public function index(Request $request)
+    {
+        $response = DB::table('items')
+            ->join('locations as L', 'items.location_id', '=', 'L.id')
+            ->join('categories as C', 'items.category_id', '=', 'C.id')
+            ->join('categories as P', 'C.parent_id', '=', 'P.id')
+            ->selectRaw('L.name as location, P.name as parent, C.name as category')
+            ->whereNull('L.deleted_at')
+            ->whereNull('C.deleted_at')
+            ->whereNull('P.deleted_at')
+            ->groupBy('L.name', 'C.name', 'P.name')
+            ->orderBy('L.name', 'asc')
+            ->get();
+
+        
+        return response()->json($response);
+    }
+}
+
+// select l.name as Location, parent.name as 'Parent Category' , c.name as Category, count(items.name) as Count
+// from items
+// left join locations l on (items.location_id = l.id)
+// left join categories c on (items.category_id = c.id)
+// left join categories parent on c.parent_id =
+// 	case 
+// 		when c.parent_id is null
+// 			then null
+// 		else parent.id
+// 		end
+// group by l.name, c.name, c.parent_id
+// order by l.name;
--- resources/assets/js/components/ReportTable.vue
+++ resources/assets/js/components/ReportTable.vue
@@ -1,9 +1,45 @@
 <template>
-  <span>report</span>
+    <div>
+        <table class="table table-striped table-bordered">
+            <thead class="thead-dark">
+                <th>Location</th>
+                <th>Parent Category</th>
+                <th>Category</th>
+                <th>Count</th>
+                <th></th>
+            </thead>
+            <tbody>
+                <tr v-for="row in results" :key="row.id">
+                    <td>{{ row.location }}</td>
+                    <td>{{ row.parent }}</td>
+                    <td>{{ row.category }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  
+    data() {
+        return {
+            price: 0,
+            results: []
+        }
+    },
+    mounted() {
+        this.getResults();
+    },
+    methods: {
+        getResults() {
+            return axios.get('/api/report')
+                .then(response => {
+                    this.results = response.data;
+                    console.log(this.results);
+                }).catch(console.error);
+        }
+    }
 }
 </script>
